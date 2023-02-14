71b785c95b278ca07b4fb650f6b033bd776e2a65
Olga Bordunova <memofpass21@gmail.com>
2023-02-14 21:30:33 +0300

    converter is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Block } from "./Block";
+import "./index.scss";
+interface Rates {
+  [index: string]: string;
+}
 function App() {
+  const [fromCurrency, setFromCurrency] = React.useState("RUB");
+  const [toCurrency, setToCurrency] = React.useState("USD");
+  const [fromPrice, setFromPrice] = React.useState(0);
+  const [toPrice, setToPrice] = React.useState(1);
+  const ratesRef = React.useRef<Rates>({});
+  React.useEffect(() => {
+    fetch(
+      "https://api.currencyfreaks.com/latest?apikey=a52a8b3a409647a8886cc39c7974b2cf"
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        ratesRef.current = json.rates;
+        onChangeToPrice(1);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Ошибка при получении");
+      });
+  }, []);
+
+  const onChangeFromPrice = (value: number) => {
+    const price = value / Number(ratesRef.current[fromCurrency]);
+    const result = price * Number(ratesRef.current[toCurrency]);
+    setToPrice(Number(result.toFixed(4)));
+    setFromPrice(value);
+  };
+  const onChangeToPrice = (value: number) => {
+    const result =
+      (Number(ratesRef.current[fromCurrency]) /
+        Number(ratesRef.current[toCurrency])) *
+      value;
+    setToPrice(value);
+    setFromPrice(Number(result.toFixed(4)));
+  };
+  React.useEffect(() => {
+    onChangeFromPrice(fromPrice);
+  }, [fromCurrency]);
+  React.useEffect(() => {
+    onChangeToPrice(toPrice);
+  }, [toCurrency]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Block
+        value={fromPrice}
+        currency={fromCurrency}
+        onChangeCurrency={setFromCurrency}
+        onChangeValue={onChangeFromPrice}
+      />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeCurrency={setToCurrency}
+        onChangeValue={onChangeToPrice}
+      />
     </div>
   );
 }
